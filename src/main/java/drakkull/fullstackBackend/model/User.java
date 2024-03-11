184aa845e20b9295eecb461ab1d549e254093614
Leonardo DiAbreu <drakkull007@hotmail.com>
2024-03-11 15:34:54 -0300

    Added User Class into model package and few annotations

---
+++ src/main/java/drakkull/fullstackBackend/model/User.java
@@ -0,0 +1,19 @@
+package drakkull.fullstackBackend.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+
+@Entity
+public class User {
+
+
+    @Id
+    @GeneratedValue
+    private Long id;
+    private String userName;
+    private String name;
+    private String email;
+
+
+}
