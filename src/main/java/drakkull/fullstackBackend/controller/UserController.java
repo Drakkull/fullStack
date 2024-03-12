b4e71ca62232e411f83abab758bbf53107c63525
Leonardo DiAbreu <drakkull007@hotmail.com>
2024-03-12 15:11:39 -0300

    Added Post and Get methods

---
+++ src/main/java/drakkull/fullstackBackend/controller/UserController.java
@@ -0,0 +1,34 @@
+package drakkull.fullstackBackend.controller;
+
+import drakkull.fullstackBackend.model.User;
+import drakkull.fullstackBackend.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class UserController {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @PostMapping("/user")
+    User newUser(@RequestBody User newUser){
+
+        return userRepository.save(newUser);
+
+    }
+
+    @GetMapping("/users")
+    List<User> getAllUsers(){
+
+        return userRepository.findAll();
+
+    }
+
+
+}
